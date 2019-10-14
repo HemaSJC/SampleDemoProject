d231819bfe1d6ac10519a74a87387a5ff2f7d607
HemalathaK <hemalathak@maveric-systems.com>
2019-10-14 17:00:49 +0530

    commit

--- Project1/src/main/java/TestBase/LaunchApplication.java
+++ Project1/src/main/java/TestBase/LaunchApplication.java
@@ -10,7 +10,21 @@ import org.testng.annotations.Test;
 
 public class LaunchApplication {
 
-	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hemalathak\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://connect.maveric-systems.com/index.php/site/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"LoginForm_username\"]")).sendKeys("hemalathak");
+		driver.findElement(By.xpath("//*[@id=\"LoginForm_password\"]")).sendKeys("maveric@1");
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[4]/input")).click();
+		Thread.sleep(2000);
+		WebElement Welcome = driver.findElement(By.xpath("//a[text()='Welcome (Hemalatha Kalliappan)']/parent::li"));
+		WebElement logout = driver.findElement(By.xpath("//a[text()='Logout ']"));
+		Actions d1 =new Actions(driver);
+		d1.moveToElement(Welcome).click(logout).build().perform();;
+		driver.close();
+	}
 	@Test
 	public void method() throws InterruptedException {
 		
@@ -32,7 +46,7 @@ public class LaunchApplication {
 	@Test
 	public void method1() throws InterruptedException {
 	 int ar[] = {1, 2, 3, 4, 5}; 
-     for (int i=0; i<=ar.length-1; i++) 
+     for (int i=0; i<=ar.length; i++) 
        System.out.println(ar[i]); 
 	}
 	
